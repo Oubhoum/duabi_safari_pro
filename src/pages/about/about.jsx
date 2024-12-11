7243d3f077fe860c9681b730d96ca4e30f6cc3aa
Abdelaziz OUBHOUM <oubhoumabdelaziz@gmail;com>
2024-12-11 16:58:46 +0100

    dubai_safari_done

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,5 +1,15 @@
+import "./about.css"
+
 const About = () => {
-    return ( <h1>About Page</h1> );
-}
+  return (
+    <div className="about">
+      <h1>About This App</h1>
+      <p>This app is all about booking safari desert tour in Dubai</p>
+      <p>
+        <strong>Version: 1.0.0</strong>
+      </p>
+    </div>
+  );
+};
 
 export default About;
