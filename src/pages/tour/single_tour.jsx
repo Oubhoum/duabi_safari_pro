342fe50e94cc563da8aff1df1c8852b0548e9702
Abdelaziz OUBHOUM <oubhoumabdelaziz@gmail;com>
2024-12-11 13:58:28 +0100

    dubai_safari_0_1

--- src/pages/tour/single_tour.jsx
+++ src/pages/tour/single_tour.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import {toursList} from "../../data";
 import Rating from "../../components/tours/rating";
 import "./single_tour.css";
+import BookNow from "./BookNow";
 
 
 const SingleTour = () => {
@@ -23,7 +24,71 @@ const SingleTour = () => {
                 Activity Provider : <span>Abdelaziz OUBHOUM</span>
             </div>
         </div>
-    </section> );
+        <div className="tour-img-wrapper">
+            <img src={tour.image} alt={tour.title} className="tour-img"/>
+            <BookNow priceFrom={tour.priceFrom}/>
+        </div>
+
+        <p className="tour-description">
+        Discover a different side of Dubai an a fun-filled safari across the
+        desert. Try sandboarding, dune bashing and a camel ride, with the option
+        to add an electifying quad bike ride or immersive Al Khayma Camp dinner
+        experience to your adventure.
+      </p>
+      <div className="tour-more-information">
+        <h2 className="tour-info-title">About this activity</h2>
+        <div className="tour-info-item">
+          <div className="tour-info-item-title">
+            <i style={{color: '#27ae60'}} className="bi bi-calendar3"></i>
+            Free Cancellation
+          </div>
+          <p className="tour-info-item-desc">
+            Cancel up to 24 hours in advance to recieve a full refund
+          </p>
+        </div>
+        <div className="tour-info-item">
+          <div className="tour-info-item-title">
+            <i style={{color: '#2980b9'}} className="bi bi-calendar-plus-fill"></i>
+            Reserve now & pay later
+          </div>
+          <p className="tour-info-item-desc">
+            Keep your travel plan flexible -- book your spot and pay nothing
+            today.
+          </p>
+        </div>
+        <div className="tour-info-item">
+          <div className="tour-info-item-title">
+            <i style={{color: '#e74c3c'}} className="bi bi-virus"></i>
+            Covid -19 precautions
+          </div>
+          <p className="tour-info-item-desc">
+            Special health and safety measures are in place. Check your activity
+            voucher once you book for full details.
+          </p>
+        </div>
+        <div className="tour-info-item">
+          <div className="tour-info-item-title">
+            <i style={{color: '#8e44ad'}} className="bi bi-clock-history"></i>
+            Duration 4 - 7 hours
+          </div>
+          <p className="tour-info-item-desc">
+          Check availability to see staring times.
+          </p>
+        </div>
+        <div className="tour-info-item">
+          <div className="tour-info-item-title">
+            <i style={{color: '#2c3e50'}} className="bi bi-person-check"></i>
+            Live tour guide
+          </div>
+          <p className="tour-info-item-desc">
+          English
+          </p>
+        </div>
+      </div>
+
+    </section> 
+    );
+
 }
 
 export default SingleTour;
